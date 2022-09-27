66a8f547693e8c714bc74cc94b27a0f0ecc9e1ed
kevin.korfmann <kevin.korfmann@gmail.com>
2022-09-27 15:17:05 +0200

    csv files and import correction

---
+++ data/smbc/old_smallpop/small/Sup_Table_1_Bottleneck_small.py
@@ -0,0 +1,31 @@
+import matplotlib
+import numpy as np
+import pandas as pd
+import scipy.special as sc
+import scipy.stats
+from matplotlib import lines as mlines
+from matplotlib import pyplot
+import math
+import msprime
+sample_size=5
+import msprime.cli as cli
+mu=1e-8
+for L in [10**7]:#
+    for r in [1]:
+        r=mu*r
+        Ne=10**4#
+        demography=msprime.Demography()
+        demography.add_population(initial_size=(Ne))
+        demography.add_population_parameters_change(time=100, growth_rate=0, initial_size=Ne/10)
+        demography.add_population_parameters_change(time=1000, growth_rate=0, initial_size=Ne)
+        for x in range(1,11):
+            #ts=msprime.DemographyDebugger(demography=demography,model=msprime.BetaCoalescent(alpha=alpha))
+            #ts.print_history()
+            ts=msprime.sim_ancestry(samples=sample_size,random_seed=((x+3)**2),recombination_rate=r,sequence_length=L,demography=demography,ploidy=2,model=[msprime.DiscreteTimeWrightFisher(duration=1000),msprime.StandardCoalescent()])
+            mts = msprime.sim_mutations(ts, rate=mu)
+            f = open("Sup_Table_1_small_Bottleneck_mu"+str(mu)+'r'+str(r)+'x'+str(x)+'L'+str(L)+".txt","w")
+            f.write("segsites: "+str(mts.get_num_sites())+'\n')
+            f.write("//"+'\n')
+            for tree in ts.trees():
+                f.write("["+str(tree.span)+"]"+str(tree.newick())+'\n')
+            f.close()
